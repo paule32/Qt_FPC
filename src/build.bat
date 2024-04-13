cd79925a50a70b3b90db9712e4e5eb9fa5bdac53
Jens Kallup <kallup-dev@web.de>
2024-04-13 16:36:52 +0200

    update

--- src/build.bat
+++ src/build.bat
@@ -4,9 +4,30 @@
 :: All rights reserved
 ::
 :: only for education, and non-profit usage !
+::
+:: Rhis batch file is optimized for a 64-Bit compilation, only.
+:: I did not test other tools for 32-Bit or operating systems other
+:: than Microsoft Windows 10/11 64-Bit Professional.
+::
+:: For compile this project you must have a minimum of:
+:: - a copy of FPC 3.2.0 64-Bit Microsoft Windows 10/11 64-Bit
+:: - a copy of NASM for Windows 10/11 (netwide assembler)
+:: - a copy of MSYS2 64-Bit with installed gcc.exe MinGW-64 Bit
+:: - a copy of msys2 tools like awk, sed, ...
+::
+:: You have to start the build.ps1 script, firstly. PowerShell will
+:: call this script with all the setting parameters.
+:: So, dont touch a running system - I give no warantees of damages
+:: or other fees.
+:: !!! YOU USE IT AT YOUR OWN RISK !!!
 :: -----------------------------------------------------------------
 ::@echo off
 setlocal enabledelayedexpansion
+
+:: -----------------------------------------------------------------
+:: developer project directory => location of the fpc-qt project ...
+:: -----------------------------------------------------------------
+set prjdir=E:\Projekte\fpc-qt\src
 
 :: -----------------------------------------------------------------
 :: fpcdir1/2 => location of fpc.exe compiler tools ...
@@ -14,12 +35,14 @@ setlocal enabledelayedexpansion
 set fpcdir1=E:\FPC\3.2.0\bin\i386-win32
 set fpcdir2=E:\fpc\3.2.2\bin\i386-win32
 
-set fpcdir=%fpcdir1%
+set gccdir1=E:\msys64\mingw32\bin
+set gccdir2=E:\msys64\mingw64\bin
 
-:: -----------------------------------------------------------------
-:: prjdir => location of the fpc-qt project ...
-:: -----------------------------------------------------------------
-set prjdir=E:\Projekte\fpc-qt\src
+set asmdir=E:\nasm
+
+set fpcdir=%fpcdir1%
+set gccdir=%gccdir2%
+set gdbdir=%gccdir2%
 
 :: -----------------------------------------------------------------
 :: parameters for fpc.exe ...
@@ -58,9 +81,8 @@ set fpcsys3=^
 :: -----------------------------------------------------------------
 :: location of nasm.exe (the netwide assembler)
 :: -----------------------------------------------------------------
-set asmdir=E:\nasm
-set asmx32=%asmdir%\nasm.exe -f win32
-set asmx64=%asmdir%\nasm.exe -f win64
+set asmx32=%asmdir%\nasm.exe -f win32 -w-orphan-labels
+set asmx64=%asmdir%\nasm.exe -f win64 -w-orphan-labels
 
 :: -----------------------------------------------------------------
 :: fpc32.exe is a copy of fpc 3.2 fpc.exe (32-Bit)
@@ -78,16 +100,28 @@ set fpcx64=%fpcdir%\fpc64.exe %fpcdst% %fpcsys2% %fpcsys1% %fpcasm%
 :: the files was manualy copied from a 32-Bit, and 64-Bit Relase
 :: -----------------------------------------------------------------
 set ld32=%fpcdir%\ld32.exe
-set ld64=%fpcdir%\ld64.exe
+set ld64=%fpcdir%\ld64.exe -b pei-x86-64 --subsystem windows
 
 :: -----------------------------------------------------------------
-:: strip32.exe is a copy of fpc 3.2 tool as.exe (32-Bit)
-:: strip64.exe is a copy of fpc 3.2 tool as.exe (64-Bit)
+:: as32.exe is a copy of fpc 3.2 tool as.exe (32-Bit)
+:: as64.exe is a copy of fpc 3.2 tool as.exe (64-Bit)
 ::
 :: the files was manualy copied from a 32-Bit, and 64-Bit Relase
 :: -----------------------------------------------------------------
 set as32=%fpcdir%\as32.exe
 set as64=%fpcdir%\as64.exe
+
+:: -----------------------------------------------------------------
+:: gcc32.exe is a copy of gcc tool gcc.exe (32-Bit)
+:: gcc64.exe is a copy of gcc tool gcc.exe (64-Bit)
+::
+:: the files was manualy copied from a 32-Bit, and 64-Bit Relase
+:: -----------------------------------------------------------------
+set gcc32=%gccdir1%\gcc.exe
+set gcc64=%gccdir2%\gcc.exe
+
+set gdb32=%gccdir1%\gdb.exe
+set gdb64=%gccdir2%\gdb.exe
 
 :: -----------------------------------------------------------------
 :: strip32.exe is a copy of fpc 3.2 tool strip.exe (32-Bit)
@@ -100,8 +134,13 @@ set strip64=%fpcdir%\strip64.exe
 
 set dlltool=%fpcdir%\dlltool.exe
 
-set srcsys=-FE%prjdir%\units\fpc-sys %prjdir%\sources\fpc-sys
-set srcrtl=-FE%prjdir%\units\fpc-rtl %prjdir%\sources\fpc-rtl
+set punits=%prjdir%\units
+set sunits=%prjdir%\sources
+
+set srcsys=-FE%punits%\fpc-sys %sunits%\fpc-sys
+set srcrtl=-FE%punits%\fpc-rtl %sunits%\fpc-rtl
+
+set sysrtl=%punits%\fpc-rtl
 
 cd %prjdir%
 
@@ -128,84 +167,78 @@ echo =[ begin compile stage    ]=
 for %%A in (fpcinit sysinit) do (
     %fpcx64% -dwindll %srcsys%\%%A.pas
 )
-%fpcx64% %fpcasm% -dwindll %srcrtl%\RTL_Utils.pas
+%fpcx64% -dwindll %srcrtl%\rtl_utils.pas
 
 echo =[ build dll file...      ]=
-%fpcx64% %fpcdst% %fpcsys2% %fpcsys1% -dwindll ^
+%fpcx64% -dwindll ^
 -Fu%prjdir%\sources\fpc-qt ^
 -FE%prjdir%\units\fpc-rtl %prjdir%\tests\fpc_rtl.pas
 if errorlevel 1 goto buildError
-goto zuzu
-cd %prjdir%\tests\units
-SET THEFILE=system
-echo Patch %THEFILE%
-for %%A in ( system fpc_rtl ) do (
-    sed -i '/\; Begin asmlist al_dwarf_frame.*/,/\; End asmlist al_dwarf_frame.*/d'         %%A.s > nul
-    sed -i '/\; Begin asmlist al_indirectglobals.*/,/\; End asmlist al_indirectglobals.*/d' %%A.s > nul
-    sed -i '/\; Begin asmlist al_globals.*/,/\; End asmlist al_globals.*/d'                 %%A.s > nul
-    sed -i '/\; Begin asmlist al_rtti.*/,/\; End asmlist al_rtti.*/d'                       %%A.s > nul
-    sed -i '/\; Begin asmlist al_const.*/,/\; End asmlist al_const.*/d'                     %%A.s > nul
-    sed -i '/File.*/d' %%A.s 2> nul
-    grep ".*lea.*\[INIT\_" %%A.s | awk '{print $2}' | sed -e "s/^.*\[//" -e "s/\]$//" -e "1d" > tmp_file
-    sort tmp_file | uniq > tmp_sort
-    del  tmp_file /F /Q > nul
-    echo SECTION .data >> %%A.s
-    for /F "tokens=*" %%b in (tmp_sort) do (
-        echo %%b: dq 0 >> %%A.s
-    )
-)
-awk "NR > 3 { print }" < %prjdir%\tests\units\system.s > system.s1
 
-echo BITS 64     >  %prjdir%\tests\units\system.s2
-echo default rel >> %prjdir%\tests\units\system.s2
-echo CPU x64     >> %prjdir%\tests\units\system.s2
-
-echo EXTERN  VMT_$SYSTEM_$$_QSTRING >> %prjdir%\tests\units\system.s2
-
-type %prjdir%\tests\units\system.s1 >> %prjdir%\tests\units\system.s2
-type %prjdir%\tests\units\system.s2 >  %prjdir%\tests\units\system.s
-
-:zuzu
 echo Assembling dll files ...
+copy %punits%\fpc-sys\fpcinit.s %sysrtl%\fpcinit.s
 
-%asmx64% -o ^
-%prjdir%\units\fpc-sys\system.o  -w-orphan-labels ^
-%prjdir%\units\fpc-sys\system.s
-
-for %%A in (fpcinit fpc_rtl) do (
-    %asmx64% -f win64 -o ^
-    %prjdir%\units\fpc-rtl\%%A.o -w-orphan-labels ^
-    %prjdir%\units\fpc-rtl\%%A.s
+:: todo => sed
+for %%A in (system fpcinit rtl_utils fpc_rtl) do (
+    %asmx64% -o %sysrtl%\%%A.o %sysrtl%\%%A.s
 )
-echo fpc_rtl.dll ...
-%ld64% -b pei-x86-64  -shared -s --dll --entry _DLLMainCRTStartup -o ^
-%prjdir%\tests\fpc_rtl.dll ^
+echo Linking fpc_rtl.dll ...
+
+%gcc64% -nostdlib -shared -o %prjdir%\tests\fpc_rtl.dll ^
+%sysrtl%\system.o    ^
+%sysrtl%\fpcinit.o   ^
+%sysrtl%\fpc_rtl.o   ^
+%sysrtl%\rtl_utils.o ^
+-L %sysrtl% -l impsystem
+
+%ld64% --shared --dll --entry _DLLMainCRTStartup -o ^
+%prjdir%\tests\fpc_rtl.dll2 ^
 %prjdir%\units\fpc-rtl\link.res
 
+::set PYTHONHOME=
+::%gdb32% %prjdir%\tests\fpc_rtl.dll
 
 echo =[ build exe file...      ]=
+::
 %fpcx64% -dwinexe -Us %srcsys%\system.pas
-for %%A in (fpintres fpcinit sysinit) do (
+%fpcx64% -dwinexe     %srcsys%\fpintres.pp
+::
+for %%A in (fpcinit sysinit) do (
     %fpcx64% -dwinexe %srcsys%\%%A.pas
 )
-%fpcx64% -dwinexe %srcrtl%\RTL_Utils.pas
-::
+%fpcx64% -dwinexe %srcrtl%\rtl_utils.pas
 %fpcx64% -dwinexe -FE%prjdir%\tests\units %prjdir%\tests\test1.pas
 
 echo Assembling exe files ...
-%asmx64% -o ^
-%prjdir%\tests\units\test1.o -w-orphan-labels ^
-%prjdir%\tests\units\test1.s
+
+copy %punits%\fpc-sys\fpcinit.s %sysrtl%\fpcinit.s
+
+:: todo => sed
+for %%A in (system fpcinit rtl_utils fpc_rtl) do (
+    %asmx64% -o %sysrtl%\%%A.o %sysrtl%\%%A.s
+)
+%asmx64% -o %prjdir%\tests\units\system.o %prjdir%\tests\units\system.s
+%asmx64% -o %prjdir%\tests\units\test1.o  %prjdir%\tests\units\test1.s
 
 echo Linking test1.exe
-copy %prjdir%\units\fpc-sys\system.o %prjdir%\tests\units\system.o
-%ld64% -b pei-x86-64 -s  --entry _mainCRTStartup -o ^
+
+%gcc64% -nostdlib -o ^
 %prjdir%\tests\test1.exe ^
+%prjdir%\tests\units\test1.o ^
+%sysrtl%\system.o    ^
+%sysrtl%\fpcinit.o   ^
+%sysrtl%\rtl_utils.o ^
+-L %prjdir%\tests\units -l impsystem -l imptest1
+
+%ld64% --entry _mainCRTStartup -o ^
+%prjdir%\tests\test2.exe ^
 %prjdir%\tests\units\link.res
-if errorlevel 1 goto linkError
+
 goto allok
-echo =[ create map ]=
-grep "FPC_move" map.txt | awk '{print $1}'
+::if errorlevel 1 goto linkError
+::goto allok
+::echo =[ create map ]=
+::grep "FPC_move" map.txt | awk '{print $1}'
 :buildError
 echo =[ build error ]=
 goto eof
